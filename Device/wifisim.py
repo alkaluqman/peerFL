eb73bb0595387aecc660bca08eb0f24392e49273
Alka Luqman <alka.luqman@gmail.com>
2022-06-22 18:47:09 +0800

    refactored ns3 communication

---
+++ Device/wifisim.py
@@ -0,0 +1,34 @@
+#import os
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import ns_helper_new as ns_helper
+import numpy as np
+import sys
+import pickle
+import time
+#import tensorflow as tf
+
+
+def oneround(data, from_node, to_node, attime=0.0):
+    ns_helper.send_data(from_node, data, attime)
+    received = ns_helper.recv_data(to_node)
+    print(received)
+    ns_helper.simulation_start()  # Force order of execution
+
+numNodes = 10
+
+ns3Nodes = ns_helper.createNodes(numNodes)
+ns3Interface = ns_helper.createInterface(ns3Nodes)
+
+in_socket = ns_helper.act_as_client(ns3Nodes, 2)
+out_socket = ns_helper.act_as_server(ns3Nodes, ns3Interface, 1, 2)
+
+fh, fm = ns_helper.monitoring_start(10.0)  # will move to class
+oneround("hello", out_socket, in_socket, 0.0)
+time.sleep(2)
+oneround("from the other side", out_socket, in_socket, 5.0)
+
+#ns_helper.monitoring_start()  # To get output from FlowMon
+
+ns_helper.monitoring_end(fh, fm)
+ns_helper.simulation_end()
+
