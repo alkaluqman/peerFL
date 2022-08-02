ac471b1f8e32838aaad8eb3a4039bacb852a8ae3
ss_sasuke1 <sahivanshu123@gmail.com>
2022-08-02 12:43:33 +0530

    updated

--- ns3_Docker/Tests/tap-wifi-virtual-machine.py
+++ ns3_Docker/Tests/tap-wifi-virtual-machine.py
@@ -30,23 +30,31 @@ cmd = ns.core.CommandLine()
 cmd.numNodes = 2
 cmd.totalTime = 600
 cmd.baseName = "Node"
+cmd.payLoad = 1472
 
 cmd.AddValue("numNodes", "Number of nodes/devices")
 cmd.AddValue("totalTime", "Total simulation time")
 cmd.AddValue("baseName", "Base Name of the Node")
-
+cmd.AddValue("payLoad", "payload of the transport layer")
 
 cmd.Parse(sys.argv)
 
 numNodes = int(cmd.numNodes)
 totalTime = int(cmd.totalTime)
 baseName = cmd.baseName
+payLoad = int(cmd.payLoad)
 
 #
 # We are interacting with the outside, real, world.  This means we have to 
 # interact in real-time and therefore we have to use the real-time simulator
 # and take the time to calculate checksums.
 #
+
+### Seting Up TCP
+#ns.core.Config.SetDefault("ns3::TcpL4Protocol::SocketType", ns.core.TypeIdValue(ns.core.TypeId("ns3::TcpNewReno")))
+ns.core.Config.SetDefault("ns3::TcpSocket::SegmentSize", ns.core.UintegerValue(payLoad))
+ns.core.Config.SetDefault("ns3::TcpL4Protocol::SocketType", ns.core.TypeIdValue(ns.internet.TcpWestwood.GetTypeId()))
+ns.core.Config.SetDefault("ns3::TcpWestwood::ProtocolType", ns.core.EnumValue(ns.internet.TcpWestwood.WESTWOODPLUS))
 ns.core.GlobalValue.Bind("SimulatorImplementationType", ns.core.StringValue("ns3::RealtimeSimulatorImpl"))
 ns.core.GlobalValue.Bind("ChecksumEnabled", ns.core.BooleanValue("true"))
 
@@ -62,14 +70,14 @@ nodes.Create (numNodes)
 # We're going to use 802.11 A so set up a wifi helper to reflect that.
 #
 wifi = ns.wifi.WifiHelper()
-wifi.SetStandard (ns.wifi.WIFI_STANDARD_80211a);
-wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager", "DataMode", ns.core.StringValue ("OfdmRate54Mbps"));
+wifi.SetStandard (ns.wifi.WIFI_STANDARD_80211a)
+wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager", "DataMode", ns.core.StringValue ("OfdmRate6Mbps"))
 
 #
 # No reason for pesky access points, so we'll use an ad-hoc network.
 #
 wifiMac = ns.wifi.WifiMacHelper()
-wifiMac.SetType ("ns3::AdhocWifiMac");
+wifiMac.SetType ("ns3::AdhocWifiMac")
 
 #
 # Configure the physical layer.
@@ -95,6 +103,12 @@ mobility.SetPositionAllocator(positionAlloc)
 mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel")
 mobility.Install(nodes)
 
+stack = ns.internet.InternetStackHelper()
+stack.Install(nodes)
+address = ns.internet.Ipv4AddressHelper()
+address.SetBase(ns.network.Ipv4Address("10.12.0.0"), ns.network.Ipv4Mask("255.255.255.0"))
+Interfaces = address.Assign(devices)
+
 #
 # Use the TapBridgeHelper to connect to the pre-configured tap devices for 
 # the left side.  We go with "UseLocal" mode since the wifi devices do not
@@ -117,8 +131,9 @@ for i in range(numNodes):
 #
 # Run the simulation for ten minutes to give the user time to play around
 #
+ns.internet.Ipv4GlobalRoutingHelper.PopulateRoutingTables()
 ns.core.Simulator.Stop (ns.core.Seconds (totalTime))
-ns.core.Simulator.Run(signal_check_frequency = -1)
+ns.core.Simulator.Run()
 ns.core.Simulator.Destroy()
 
 
