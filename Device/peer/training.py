19fb9e89e9115a7be54a597ef6dd8597fed3d681
ss_sasuke1 <sahivanshu123@gmail.com>
2023-02-09 22:09:59 +0530

    Added MobileNet

--- Device/peer/training.py
+++ Device/peer/training.py
@@ -11,6 +11,7 @@ from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
 import joblib
 
 class SimpleMLP:
@@ -18,6 +19,10 @@ class SimpleMLP:
     @staticmethod
     def build(shape, classes, only_digits=True):
         #base_model_1 = VGG16(include_top=False, input_shape=shape, classes=classes)
+        #base_model_1 = MobileNetV2(include_top=False, input_shape=shape, classes=classes)
+        #base_model_1.trainable = False
+        base_model_1 = tf.keras.models.load_model('/usr/thisdocker/dataset/base_model')
+        base_model_1.trainable = False
         model_1 = Sequential()
         #model_1.add(base_model_1)  # Adds the base model (in this case vgg19 to model_1)
         model_1.add(
@@ -27,10 +32,10 @@ class SimpleMLP:
         #model_1.add(Dense(1024, activation=('relu'), input_dim=512))
         #model_1.add(Dense(512, activation=('relu')))
         #model_1.add(Dense(256, activation=('relu')))
-        ## model_1.add(Dropout(.3))#Adding a dropout layer that will randomly drop 30% of the weights
+        #model_1.add(Dropout(.3))#Adding a dropout layer that will randomly drop 30% of the weights
         model_1.add(Dense(128, activation=('relu')))
         # model_1.add(Dropout(.2))
-        model_1.add(Dense(10, activation=('softmax')))  # This is the classification layer
+        model_1.add(Dense(classes, activation=('softmax')))  # This is the classification layer
         return model_1
 
 
@@ -113,6 +118,33 @@ def local_training(client_num, local_model, build_flag):
     # Training
     local_model.fit(local_dataset, epochs=1, verbose=1)
     print("%sDone" % log_prefix)
+
+    # Save model - moved to node
+    return local_model
+
+def build_model(client_num, local_model, build_flag):
+    # Load client dataset from volume mounted folder
+    # client_num = 1
+    log_prefix = "[" + str(client_num).upper() + "] "
+    local_dataset = load_client_dataset()
+    x = local_dataset.element_spec[0].shape[1]
+    y = local_dataset.element_spec[0].shape[2]
+    z = local_dataset.element_spec[0].shape[3]
+    input_shape = (x, y, z)
+    num_classes = local_dataset.element_spec[1].shape[1]
+
+    if build_flag:
+        # Create model
+        lr = 0.01
+        loss = 'categorical_crossentropy'
+        metrics = ['accuracy']
+        optimizer = SGD(lr=lr, decay=lr, momentum=0.9)
+
+        print("%sBuilding model ..." % log_prefix)
+        smlp_local = SimpleMLP()
+        local_model = smlp_local.build(shape=input_shape, classes=num_classes)
+        local_model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+        local_model.build(input_shape=(None, x, y, z))
 
     # Save model - moved to node
     return local_model
